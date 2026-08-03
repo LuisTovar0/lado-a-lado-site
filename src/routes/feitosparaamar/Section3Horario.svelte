9dafec9356ca167c48dcddfd8b2059af03437747
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2026-08-03 17:18:48 +0100

    fix: simplify schedule responsiveness

--- src/routes/feitosparaamar/Section3Horario.svelte
+++ src/routes/feitosparaamar/Section3Horario.svelte
@@ -1,16 +1,15 @@
-<script>
-    import {vw} from "$lib";
+﻿<script>
 </script>
 
 <section class="md:gap-4 md:py-12 flex flex-col items-center gap-4 py-10">
-    <h1 class="color-red mono">Horário</h1>
+    <h1 class="color-red mono">HorÃ¡rio</h1>
     <h2 class="fruits-delight text-center text-3xl md:text-4xl xl:text-5xl">Que programa temos para oferecer?</h2>
-    <div class="horário flex flex-col w-11/12 max-w-[900px]">
+    <div class="horÃ¡rio flex flex-col w-11/12 max-w-[900px]">
         <div class="color-red mono !items-end">
-            <span class="text-lg font-semibold w-[70%]">SÁBADO, 6 ABRIL</span>
+            <span class="text-lg font-semibold w-[70%]">SÃBADO, 6 ABRIL</span>
             <div class="flex flex-col items-end">
                 <span>Lisboa</span>
-                <span class="text-right">Universidade Católica</span>
+                <span class="text-right">Universidade CatÃ³lica</span>
             </div>
         </div>
         <hr class="!border-t-2 !border-[var(--theme-red)]" />
@@ -32,9 +31,7 @@
             <div>
                 <span></span>
                 <span>O mundo em que vivemos</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
+                <div class="hidden md:block"></div>
                 <span>Francisco Vilhena da Cunha</span>
             </div>
             <div>
@@ -48,9 +45,7 @@
             <div>
                 <span></span>
                 <span>Para que fui feito?</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
+                <div class="hidden md:block"></div>
                 <span>Pe. Bernardo Aranha</span>
             </div>
             <div>
@@ -64,14 +59,12 @@
             <div>
                 <span></span>
                 <span>A beleza da complementaridade</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
-                <span>Javier Calderón e Alexandra Chumbo</span>
+                <div class="hidden md:block"></div>
+                <span>Javier CalderÃ³n e Alexandra Chumbo</span>
             </div>
             <div class="flex">
                 <div class="over">
-                    <img src="/img/fpa/javi.png" alt="Javier Calderón" class="javi" />
+                    <img src="/img/fpa/javi.png" alt="Javier CalderÃ³n" class="javi" />
                 </div>
                 <div>
                     <img src="/img/fpa/alex.png" alt="Alexandra Chumbo" />
@@ -84,16 +77,14 @@
                 <span></span>
                 <span>Coffee break (30 min)</span>
             </div>
-            <span class="text-3xl">☕️</span>
+            <span class="text-3xl">â˜•ï¸</span>
         </div>
         <hr />
         <div class="momento">
             <div>
                 <span></span>
                 <span>Do encantamento inicial ao amor</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
+                <div class="hidden md:block"></div>
                 <span>Rodolfo Nona</span>
             </div>
             <div>
@@ -107,15 +98,13 @@
             <div>
                 <span></span>
                 <span>Amar-te todos os dias da minha vida</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
-                <span>Graça e Miguel Varão</span>
+                <div class="hidden md:block"></div>
+                <span>GraÃ§a e Miguel VarÃ£o</span>
             </div>
             <div>
                 <div>
                     <img class="relative left-1/2 -translate-x-1/2 scale-110 top-1" src="/img/fpa/mig.png"
-                         alt="Graça e Miguel Varão" />
+                         alt="GraÃ§a e Miguel VarÃ£o" />
                 </div>
             </div>
         </div>
@@ -124,9 +113,7 @@
             <div>
                 <span></span>
                 <span>Fui ao fundo. E agora?</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
+                <div class="hidden md:block"></div>
                 <span>Vasco Almeida Ribeiro</span>
             </div>
             <div>
@@ -140,15 +127,13 @@
             <div>
                 <span></span>
                 <span>"Ama e faz o que quiseres"</span>
-                {#if ($vw >= 768)}
-                    <div></div>
-                {/if}
-                <span>Fátima e Henrique Fonseca</span>
+                <div class="hidden md:block"></div>
+                <span>FÃ¡tima e Henrique Fonseca</span>
             </div>
             <div>
                 <div>
                     <img class="relative scale-[1.4] bottom-4 right-1" src="/img/fpa/fati.png"
-                         alt="Fátima e Henrique Fonseca" />
+                         alt="FÃ¡tima e Henrique Fonseca" />
                 </div>
             </div>
         </div>
@@ -157,7 +142,7 @@
 </section>
 
 <style lang="scss">
-    .horário {
+    .horÃ¡rio {
         & > div {
             @apply flex items-center justify-between my-2;
         }
@@ -181,9 +166,7 @@
                     font-family: OverpassMono, monospace;
                     color: var(--theme-red);
 
-                    @media (min-width: 768px) {
-                        @apply font-bold text-black;
-                    }
+                    @apply md:font-bold md:text-black;
                 }
 
                 & > span:nth-child(2) {
